17e51b7e5ddb0db111a9ec3f20889b8563e317b5
sxudai <46526181+sxudai@users.noreply.github.com>
2021-05-13 15:05:38 +0800

    完善http支持
    完善了hppt协议的支持
    增加了post方法，功能暂定为简单将报文实体打印置屏幕

--- request.cpp
+++ request.cpp
@@ -1,32 +1,32 @@
-#include "request.h"
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include<string.h>
+
+#include <unistd.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <sys/epoll.h>
-#include <ctype.h>
-#include<string.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <netinet/in.h>
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+
+#include <string>
 #include <thread>
-
 #include <iostream>
-
+#include <unordered_map>
 
 #include "util.h"
+#include "request.h"
 
 using std::cout;
 using std::endl;
 
-uniRequest::uniRequest(int _lfd, int _cfd, int _efd): m_lfd(_lfd), m_cfd(_cfd), m_efd(_efd){}
-uniRequest::uniRequest(){
-    m_lfd = -1; m_cfd = -1; m_efd = -1;
-}
+uniRequest::uniRequest(int _lfd, int _cfd, int _efd): m_lfd(_lfd), m_cfd(_cfd), m_efd(_efd), m_error(dsx::OK){}
+uniRequest::uniRequest(): m_lfd(0), m_cfd(0), m_efd(0), m_error(dsx::invalidDefaultConstructor){}
 uniRequest::~uniRequest(){
     cout << "go to destory" << endl;
 }
@@ -63,69 +63,89 @@ int uniRequest::acceptLink(){
 int uniRequest::epollIn(){
     cout << "handle cfd epoll in, pid is: " << std::this_thread::get_id() << endl;
 
-    // 检测用返回值
-    int res;
+	analysis_request();
+	if(m_error != dsx::OK) return disconnect();
+	cout << "analysis_request end" << endl;
+	for(auto & p: m_heads) cout << p.first << ": " << p.second << endl;
 
-    // 读取一行http协议， 拆分， 获取 get 文件名 协议号	
-	char line[1024] = {0};
-	char method[16], path[256], protocol[16]; 
+	http_request();
 	
-	int len = get_line(m_cfd, line, sizeof(line)); //读 http请求协议首行 GET /hello.c HTTP/1.1
-
-    if(len == 0){  // cfd连接关闭
-        res = epoll_ctl(m_efd, EPOLL_CTL_DEL, m_cfd, NULL);
-        close(m_cfd);
-        printf("client[%d] closed connection\n", m_cfd);
-        delete this;
-        return 0;
-    } else{
-		sscanf(line, "%[^ ] %[^ ] %[^ ]", method, path, protocol);	
-		printf("method=%s, path=%s, protocol=%s\n", method, path, protocol);
-		while (1) {
-			char buf[1024] = {0};
-			len = get_line(m_cfd, buf, sizeof(buf));	
-			if (buf[0] == '\n') {
-				break;	
-			} else if (len == -1)
-				break;
-		}	
-	}
-
-    if (strncasecmp(method, "GET", 3) == 0){
-		
-		http_request(path);
-
-        //http协议，交流完一次后，断开连接
-        res = epoll_ctl(m_efd, EPOLL_CTL_DEL, m_cfd, NULL);
-        printf("client[%d] closed connection\n", m_cfd);
-        close(m_cfd);
-	}
-    delete this;
-    return 0;
+    return disconnect();
 }
 
-void uniRequest::http_request(const char *file)
-{
-	struct stat sbuf;
+void uniRequest::analysis_request(){
+    // 读取一行http协议， 拆分， 获取 get 文件名 协议号	
+	char line[1024], method[16], path[256], protocol[16]; 
+	memset(line, 0, sizeof(line));
+	memset(method, 0, sizeof(method));
+	memset(path, 0, sizeof(path));
+	memset(protocol, 0, sizeof(protocol));
 	
+	int len = get_line(m_cfd, line, sizeof(line)); //读http请求协议首行 GET /hello.c HTTP/1.0
+	// cfd连接关闭
+    if(len == 0){  
+        m_error = dsx::anaRequestErrCfdClose;
+        return;
+    }
+
+	sscanf(line, "%[^ ] %[^ ] %[^ ]", method, path, protocol);
+	printf("method=%s, path=%s, protocol=%s\n", method, path, protocol);
+	m_path = '.';
+	m_method = method; m_path += path; m_protocol = protocol;
+	while(true){
+		memset(line, 0, sizeof(line));
+		len = get_line(m_cfd, line, sizeof(line));	
+		if (len == -1 || line[0] == '\0') break;
+		char key[32], value[512];
+		sscanf(line, "%[^: ]%*[: ]%[^ ]", key, value);
+		m_heads.emplace(key, value);
+	}
+	while(true){
+		memset(line, 0, sizeof(line));
+		len = get_line(m_cfd, line, sizeof(line));
+		// len == -1说明body没了
+		if(len == -1) break;
+		m_body += line;
+		m_body += "\n";
+	}
+	return;
+}
+
+void uniRequest::http_request()
+{
+	if(m_method == "GET"){
+		http_get();
+	} else if(m_method == "POST"){
+		http_post();
+	}
+}
+
+void uniRequest::http_get(){
+	cout << "m_method: get" << m_body << endl;
+	struct stat sbuf;
 	// 判断文件是否存在
-	int ret = stat(file, &sbuf);
+	int ret = stat(m_path.c_str(), &sbuf);
 	if (ret != 0) {
 		// 回发浏览器 404 错误页面
         send_respond(404, "notFound", "Content-Type:html", -1);
 		send_file("./404notFund.html");
+		m_error = dsx::httpGetErr404;
+		return;
 	}
-	
+
 	if(S_ISREG(sbuf.st_mode)) {		// 是一个普通文件
 		
-		// 回发 http协议应答
-		//send_respond(cfd, 200, "OK", " Content-Type: text/plain; charset=iso-8859-1", sbuf.st_size);	 
-		send_respond(200, "OK", "Content-Type:image/jpeg", -1);
-		//send_respond(cfd, 200, "OK", "audio/mpeg", -1);
+		// 回发 http协议应答 
+		send_respond(200, "OK", "Content-Type: image/jpeg", -1);
 		
 		// 回发 给客户端请求数据内容。
-		send_file(file);
-	}	
+		send_file(m_path.c_str());
+	}
+}
+
+void uniRequest::http_post(){
+	cout << "m_method: post, receive content:" << m_body << endl;
+	send_respond(200, "OK", "Content-Type: text/plain", -1);
 }
 
 void uniRequest::send_respond(int no, const char *disp, const char *type, int len)
@@ -150,8 +170,7 @@ void uniRequest::send_file(const char *file)
 	// 打开的服务器本地文件。  --- cfd 能访问客户端的 socket
 	int fd = open(file, O_RDONLY);
 	if (fd == -1) {
-		file = "./404notFund.html";
-        fd = open(file, O_RDONLY);
+        fd = open("./404notFund.html", O_RDONLY);
 	}
 	
 	while ((n = read(fd, buf, sizeof(buf))) > 0) {		
@@ -163,4 +182,14 @@ void uniRequest::send_file(const char *file)
 		if (ret < 4096)		
 			printf("-----send ret: %d\n", ret);
 	}
+}
+
+int uniRequest::disconnect(){
+	int returnVal=0;
+	int res = epoll_ctl(m_efd, EPOLL_CTL_DEL, m_cfd, NULL);
+	printf("client[%d] closed connection\n", m_cfd);
+	close(m_cfd);
+	if(m_error != dsx::OK) return returnVal=-1;
+	delete this;
+	return returnVal;
 }
--- request.h
+++ request.h
@@ -1,5 +1,18 @@
 #pragma once
 
+#include <string>
+#include <unordered_map>
+
+namespace dsx{
+    enum myErrorNum{
+        OK=0,
+        invalidDefaultConstructor=1,
+        anaRequestErrCfdClose=2,
+        httpGetErr404=3
+    };
+}
+
+
 struct uniRequest{
 public:
     uniRequest();
@@ -11,9 +24,14 @@ public:
     int acceptLink();
 
     int epollIn();
-    void http_request(const char *file);
+    void analysis_request();
+    void http_request();
+    void http_get();
+    void http_post();
     void send_file(const char *file);
     void send_respond(int no, const char *disp, const char *type, int len);
+
+    int disconnect();
 
     int get_efd();
     int get_cfd();
@@ -22,4 +40,13 @@ private:
     int m_lfd;
     int m_cfd;
     int m_efd;
+
+    std::string m_method;
+    std::string m_path;
+    std::string m_protocol;
+    std::string m_body;
+
+    dsx::myErrorNum m_error;
+
+    std::unordered_map<std::string, std::string> m_heads;
 };
