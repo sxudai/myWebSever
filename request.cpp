8bb5deb5c2c79814e7af70f936bcf3fba2c9eace
sxudai <sxudai@163.com>
2021-08-13 13:17:15 +0800

    update

--- request.cpp
+++ request.cpp
@@ -480,6 +480,7 @@ void uniRequest::send_file(const char *file){
 			if(ret > 0){
 				retryCnt = 0;
 				hadSended += ret;
+				printf("success send %d bytes, total %d bytes\n", hadSended,  n); 
 			} else {
 				printf("error, try again, n is %d, ret is %d, %s\n", n, ret, strerror(errno)); 
 				++retryCnt;
