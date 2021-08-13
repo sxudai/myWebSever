191d41e1f2ebebce7309c1295c951646f4db02e6
sxudai <sxudai@163.com>
2021-08-13 10:40:52 +0800

    add some debug cout

--- request.cpp
+++ request.cpp
@@ -471,15 +471,15 @@ void uniRequest::send_file(const char *file){
 	}
 	
 	while ((n = read(fd, buf, sizeof(buf))) > 0) {
-		// int try_to_read = TRY_READ;
 		int cnt = 0;
 		do{
 			std::this_thread::sleep_for(std::chrono::seconds(cnt));
 			ret = send(m_cfd, buf, n, 0);
-			// --try_to_read;
 			++cnt;
 			if(cnt == 6) break;
-			// if(ret > 0) try_to_read = TRY_READ;
+			if(ret > 0) {
+				cout << "send " << ret << "bytes, success in cnt = " << cnt << '\n';
+			}
 		}while(ret == -1); 
 	}
 	close(fd);
