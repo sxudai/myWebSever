66839ea752787c543208eb1149502bc3a9882751
sxudai <sxudai@163.com>
2021-08-13 10:23:14 +0800

    modify send file logic

--- request.cpp
+++ request.cpp
@@ -471,18 +471,16 @@ void uniRequest::send_file(const char *file){
 	}
 	
 	while ((n = read(fd, buf, sizeof(buf))) > 0) {
-		int try_to_read = TRY_READ;
+		// int try_to_read = TRY_READ;
+		int cnt = 0;
 		do{
+			std::this_thread::sleep_for(std::chrono::seconds(cnt));
 			ret = send(m_cfd, buf, n, 0);
-			--try_to_read;
-			if(try_to_read < 0) break;
-			if(ret > 0) try_to_read = TRY_READ;
-		}while(ret == -1); //{
-		// 	printf("client closed: %d\n", m_cfd);
-		// 	// break;
-		// }
-		// }
-		// if (ret < 4096) printf("-----send ret: %d\n", ret);
+			// --try_to_read;
+			++cnt;
+			if(cnt == 6) break;
+			// if(ret > 0) try_to_read = TRY_READ;
+		}while(ret == -1); 
 	}
 	close(fd);
 }
